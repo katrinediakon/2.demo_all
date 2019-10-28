82042a707148df09252db74de46554753c140e13
Kate <Katrinekatek@gmail.com>
2019-10-28 13:57:46 +0300

    ex2-95

--- local/php_interface/include/menu.php
+++ local/php_interface/include/menu.php
@@ -4,15 +4,18 @@ AddEventHandler("main", "OnBuildGlobalMenu", "MyOnBuildGlobalMenu");
 
 function MyOnBuildGlobalMenu(&$aGlobalMenu, &$aModuleMenu)
 {
-    // Убрать "Рабочий стол"
-    unset($aGlobalMenu["global_menu_desktop"]);
-    unset($aGlobalMenu["global_menu_services"]);
-    unset($aGlobalMenu["global_menu_marketplace"]);
-    unset($aGlobalMenu["global_menu_settings"]);
-    foreach ($aModuleMenu as $menu) {
-        if ($menu["text"] == "Новости") {
-            $aModuleMenu = null;
-            $aModuleMenu[0] = $menu;
+    global $USER;
+    if( array_search(6,CUser::GetUserGroup($USER->GetID()))) {
+        //Убрать "Рабочий стол"
+        unset($aGlobalMenu["global_menu_desktop"]);
+        unset($aGlobalMenu["global_menu_services"]);
+        unset($aGlobalMenu["global_menu_marketplace"]);
+        unset($aGlobalMenu["global_menu_settings"]);
+        foreach ($aModuleMenu as $menu) {
+            if ($menu["text"] == "Новости") {
+                $aModuleMenu = null;
+                $aModuleMenu[0] = $menu;
+            }
         }
     }
 }
