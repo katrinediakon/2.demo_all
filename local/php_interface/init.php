7eff061b963c074a61b18dd0dc369c561efec3b1
Kate <Katrinekatek@gmail.com>
2019-10-28 12:52:22 +0300

    ex2-93

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -2,3 +2,6 @@
 
 if(file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/product.php"))
     require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/product.php");
+
+if(file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/error.php"))
+    require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/error.php");
