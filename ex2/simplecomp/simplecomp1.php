0c7b701e93bd9033848650386907d82b024f1120
Kate <Katrinekatek@gmail.com>
2019-10-28 16:35:23 +0300

    ex2-70

---
+++ ex2/simplecomp/simplecomp1.php
@@ -0,0 +1,15 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Простой компонент 1");
+?><?$APPLICATION->IncludeComponent(
+	"mycomponent:simplecomp.exam1",
+	"",
+	Array(
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CAT_IBLOCK_ID" => "",
+		"CODE" => "UF_NEWS_LINK",
+		"NEWS_IBLOCK_ID" => "1",
+		"PRODUCTS_IBLOCK_ID" => "2"
+	)
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ local/components/mycomponent/simplecomp.exam1/.parameters.php
@@ -0,0 +1,19 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+$arComponentParameters = array(
+	"PARAMETERS" => array(
+		"PRODUCTS_IBLOCK_ID" => array(
+			"NAME" => GetMessage("CAT_IBLOCK_ID"),
+			"TYPE" => "STRING",
+		),
+        "NEWS_IBLOCK_ID" => array(
+            "NAME" => GetMessage("NEWS_IBLOCK_ID"),
+            "TYPE" => "STRING",
+        ),
+        "CODE" => array(
+            "NAME" => GetMessage("CODE"),
+            "TYPE" => "STRING",
+        ),
+        "CACHE_TIME"  =>  array("DEFAULT"=>36000000),
+	),
+);
