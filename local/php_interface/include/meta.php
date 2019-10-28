1e364415100b841dd2d75fd607bd68542a550361
Kate <Katrinekatek@gmail.com>
2019-10-28 14:04:50 +0300

    ex2-94

---
+++ local/php_interface/include/meta.php
@@ -0,0 +1,16 @@
+<?
+AddEventHandler('main', 'OnEpilog', 'META', 1);
+
+function META()
+{
+    if (CModule::IncludeModule('iblock')) {
+        global $APPLICATION;
+        $arSelect = Array("ID", "NAME", "PROPERTY_TITLE", "PROPERTY_DESCRIPTION");
+        $arFilter = Array("IBLOCK_ID" => 6, "NAME" => $APPLICATION->GetCurPage(), "ACTIVE" => "Y");
+        $res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize" => 50), $arSelect);
+        while ($ob = $res->GetNext()) {
+            $APPLICATION->SetTitle($ob["PROPERTY_TITLE_VALUE"]);
+            $APPLICATION->SetPageProperty("description", $ob["PROPERTY_DESCRIPTION_VALUE"]);
+        }
+    }
+}
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -11,3 +11,6 @@ if(file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/feedback.
 
 if(file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/menu.php"))
     require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/menu.php");
+
+if(file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/meta.php"))
+    require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/meta.php");
