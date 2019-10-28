6af045d54db2ec9ade3d7cfca4ce0ba4dc24ec41
Kate <Katrinekatek@gmail.com>
2019-10-28 12:28:58 +0300

    ex2-50

---
+++ local/php_interface/include/product.php
@@ -0,0 +1,26 @@
+<? // регистрируем обработчик
+AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("MyClass", "OnBeforeIBlockElementUpdateHandler"));
+
+class MyClass
+{
+    // создаем обработчик события "OnBeforeIBlockElementUpdate"
+    function OnBeforeIBlockElementUpdateHandler(&$arFields) {
+
+        if ($arFields["IBLOCK_ID"] == 2 && $arFields["ACTIVE"] == 'N') {
+            global $APPLICATION;
+            $count;
+            $arSelect = Array("SHOW_COUNTER");
+            $arFilter = Array("ID" => $arFields["ID"]);
+            $res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>50), $arSelect);
+            while($ob = $res->GetNext()) {
+                $count=$ob['SHOW_COUNTER'];
+            }
+            if($count>=2) {
+                $APPLICATION->throwException("Товар невозможно деактивировать, у него ". $count ." просмотров");
+                return false;
+            }
+        }
+    }
+}
+
+?>
