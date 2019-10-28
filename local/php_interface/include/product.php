0029ac5326d72f7ef73e006658db5328fb0d53f4
Kate <Katrinekatek@gmail.com>
2019-10-28 12:42:38 +0300

    ex2-50

--- local/php_interface/include/product.php
+++ local/php_interface/include/product.php
@@ -4,19 +4,19 @@ AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("MyClass", "OnBef
 class MyClass
 {
     // создаем обработчик события "OnBeforeIBlockElementUpdate"
-    function OnBeforeIBlockElementUpdateHandler(&$arFields) {
+    function OnBeforeIBlockElementUpdateHandler(&$arFields)
+    {
 
         if ($arFields["IBLOCK_ID"] == 2 && $arFields["ACTIVE"] == 'N') {
             global $APPLICATION;
-            $count;
             $arSelect = Array("SHOW_COUNTER");
             $arFilter = Array("ID" => $arFields["ID"]);
-            $res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>50), $arSelect);
-            while($ob = $res->GetNext()) {
-                $count=$ob['SHOW_COUNTER'];
+            $res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize" => 50), $arSelect);
+            while ($ob = $res->GetNext()) {
+                $count = $ob['SHOW_COUNTER'];
             }
-            if($count>=2) {
-                $APPLICATION->throwException("Товар невозможно деактивировать, у него ". $count ." просмотров");
+            if ($count >= 2) {
+                $APPLICATION->throwException("Товар невозможно деактивировать, у него " . $count . " просмотров");
                 return false;
             }
         }
