4e9e787822bd76fbe2bf866a304e62a9dd5d303b
Kate <Katrinekatek@gmail.com>
2019-10-29 15:04:42 +0300

    ex2-25

--- ex2/.left.menu.php
+++ ex2/.left.menu.php
@@ -13,6 +13,20 @@ $aMenuLinks = Array(
 		Array(), 
 		Array(), 
 		"" 
+	),
+	Array(
+		"Оценка производительности", 
+		"/ex2/time_control/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Комплексный компонент", 
+		"/ex2/complexcomponent/", 
+		Array(), 
+		Array(), 
+		"" 
 	)
 );
 ?>
